9845846a17f725bde79a7a2b2ab26fc42d66e96b
James Demlow <jdemlow@gmail.com>
2023-05-30 15:16:05 -0600

    working on mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,17 +20,17 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div className="md:hidden">
+      <div className="md:hidden z-10">
         <FaBars />
       </div>
 
       {/* Mobile Menu */}
-      <ul className="absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul className="absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/* Social Icons */}
